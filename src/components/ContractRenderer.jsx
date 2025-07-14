278f9c9473eb9c98366ee1306ad6c8b445b548a2
Khoa Le <rubikvn2100@gmail.com>
2025-07-14 14:59:04 -0700

    Add simple type render.

--- src/components/ContractRenderer.jsx
+++ src/components/ContractRenderer.jsx
@@ -23,7 +23,44 @@ function ContractRenderer({ contractConfig }) {
 
   const children = contractConfig.children ? renderNodes(contractConfig.children) : null;
 
-  return children;
+  switch (contractConfig.type) {
+    case 'clause':
+      return (
+        <div className="clause" style={getStyles(contractConfig)}>
+          {children}
+        </div>
+      );
+    case 'mention':
+      return (
+        <span
+          className="mention"
+          style={{ ...getStyles(contractConfig), backgroundColor: contractConfig.color }}
+          title={contractConfig.title}
+        >
+          {contractConfig.value || (contractConfig.children && contractConfig.children[0]?.text)}
+        </span>
+      );
+    case 'block':
+      return <div style={getStyles(contractConfig)}>{children}</div>;
+    case 'p':
+      return <p style={getStyles(contractConfig)}>{children}</p>;
+    case 'h1':
+      return <h1 style={getStyles(contractConfig)}>{children}</h1>;
+    case 'h2':
+      return <h2 style={getStyles(contractConfig)}>{children}</h2>;
+    case 'h3':
+      return <h3 style={getStyles(contractConfig)}>{children}</h3>;
+    case 'h4':
+      return <h4 style={getStyles(contractConfig)}>{children}</h4>;
+    case 'ul':
+      return <ul style={getStyles(contractConfig)}>{children}</ul>;
+    case 'li':
+      return <li style={getStyles(contractConfig)}>{children}</li>;
+    case 'lic':
+      return <li style={getStyles(contractConfig)}>{children}</li>;
+    default:
+      return <div style={getStyles(contractConfig)}>{children}</div>;
+  }
 }
 
 export default ContractRenderer;
